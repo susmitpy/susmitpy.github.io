d696452b4a974d16a69243d3c978d90a4a9e4d51
Susmit Vengurlekar <susmit.py@gmail.com>
2025-01-19 11:21:31 +0530

    update
    Signed-off-by: Susmit Vengurlekar <susmit.py@gmail.com>

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -5,7 +5,7 @@ import { SkillsData } from '@/lib/data';
 export default function SkillsSection() {
 
     return (
-        <div className='py-8 bg-[#2f2f2f]'>
+        <div className='py-20 bg-[#2f2f2f] min-h-[600px]'>
             <span className="block text-center text-2xl font-bold py-2 text-[#40E0D0]">{SkillsData.heading}</span>
             {Object.values(SkillsData.skillGroups).map((skills, index) => (
                 <MovingSkills key={index} items={skills} />
